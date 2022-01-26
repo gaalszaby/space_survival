55c8ee8593a42a5c551e5e15d3e34cdcd346fa82
gaalszaby <gaalszaby@outlook.com>
2022-01-26 11:15:22 +0100

    project start

---
+++ Assets/bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("DestroySelf", 3f);
+    }
+    public GameObject hiteffect;
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("megutotte");
+        GameObject effect = Instantiate(hiteffect, transform.position, Quaternion.identity);
+       if (collision.gameObject.CompareTag("enemy")) {
+            Destroy(effect, 2f);
+            Destroy(gameObject);
+       
+
+        }
+       
+    }
+}
---
+++ Assets/shooting.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bulletprefab;
+    public float cooldown = 5f;
+    float nextfire;
+
+    public float bulletforce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    
+
+        
+        {
+            Shoot();
+        }
+    void Shoot()
+    {
+        if (Time.time > nextfire)
+        {
+            nextfire = Time.time + cooldown;
+            GameObject bullet = Instantiate(bulletprefab, firepoint.position, firepoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firepoint.up * bulletforce, ForceMode2D.Impulse);
+        }
+
+        {
+            
+        }
+    }
+
+
+
+
+}
